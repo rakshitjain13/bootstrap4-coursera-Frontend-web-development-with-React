d1be5bfac0150d6eb0d13198a2f809a8ff6efb96
Rakshit Jain <rakshitdhoka13@gmail.com>
2020-06-03 00:51:24 +0530

    Npm scripts -1

---
+++ js/scrips.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselButton").click(function(){
+            if ($("#carouselButton").children("span").hasClass('fa-pause')) {
+                $("#mycarousel").carousel('pause');
+                $("#carouselButton").children("span").removeClass('fa-pause');
+                $("#carouselButton").children("span").addClass('fa-play');
+            }
+            else if ($("#carouselButton").children("span").hasClass('fa-play')){
+                $("#mycarousel").carousel('cycle');
+                $("#carouselButton").children("span").removeClass('fa-play');
+                $("#carouselButton").children("span").addClass('fa-pause');                    
+            }
+        });
+    $(".loginbtn").click(function(){
+        $('#loginModal').modal('toggle');
+    });
+    $(".btnreserve").click(function(){
+        $('#modalreserve').modal('toggle');
+    });
+});
